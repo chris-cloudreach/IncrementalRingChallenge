b02e39ddb37910e0f26406275a160fe1bb3d044f
Christopher <christopher.ogbunuzor@cloudreach.com>
2021-11-30 09:40:47 +0000

    Completing this challenge

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Opening number.txt, assuming it is in the same folder as the script, and incrementing the number in it
+value=`cat number.txt`
+new_value=`expr $value + 1`
+echo $new_value > number.txt
+
+# Copying the file to the next server
+scp -i number-ring-key.pem /home/ec2-user/number.txt ec2-user@192.168.22.177:/home/users/ec2-user/
+
